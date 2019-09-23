684808a98d21b8f2849446705b21e85fd3a7b915
quietdw <1044569102@qq.com>
2019-09-23 11:34:59 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,62 @@
 import React from "react"
 import ReactDOM from "react-dom" // 虚拟Dom
-import ChessBoard from "./ChessBoard"
 
-ReactDOM.render(<ChessBoard />, document.getElementById("root"))
-// {}
+import { createStore } from "redux"
+
+function counter(state, action) {
+  state = state || { number: 0 }
+
+  switch (action.type) {
+    case "INCREMENT":
+      return { number: state.number + 1 }
+    case "DECREMENT":
+      return { number: state.number - 1 }
+    default:
+      return { number: state.number }
+  }
+}
+
+let store = createStore(counter)
+
+class TestStore extends React.Component {
+  add() {
+    store.dispatch({ type: "INCREMENT" })
+  }
+
+  minus() {
+    store.dispatch({ type: "DECREMENT" })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.store.number}
+        <button
+          onClick={() => {
+            this.add()
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            this.minus()
+          }}
+        >
+          -
+        </button>
+      </div>
+    )
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TestStore store={store.getState()} />,
+    document.getElementById("root")
+  )
+}
+
+render()
+
+store.subscribe(render)
